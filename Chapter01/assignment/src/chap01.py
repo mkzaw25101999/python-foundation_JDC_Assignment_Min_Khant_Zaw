24047d6b9f1b93f281e593f557677814d26a96a8
Min Zaw <khantzawkz23@gmail.com>
2026-01-11 11:45:36 +0200

    Uploading Chapter 01 assignment

---
+++ Chapter01/assignment/src/chap01.py
@@ -0,0 +1,4 @@
+#Chapter01 - Assignment
+
+def say_hello(name : str) -> str:
+    return f"Hello, {name}!"
---
+++ Chapter01/assignment/tests/test_chap01.py
@@ -0,0 +1,5 @@
+
+from src.chap01 import say_hello
+
+def test_say_hello():
+    assert say_hello("Min Khant Zaw") == "Hello, Min Khant Zaw!"
