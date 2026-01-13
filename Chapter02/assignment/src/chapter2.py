3f0f7312ed490a74f8946d92a2db952be84f68e0
Min Zaw <khantzawkz23@gmail.com>
2026-01-13 18:02:45 +0200

    Chapter 2 assignment update

--- Chapter02/assignment/src/chapter2.py
+++ Chapter02/assignment/src/chapter2.py
@@ -5,17 +5,22 @@ def create_person(name: str, age: int, height: float, student: bool) -> dict:
     Create a dictionary with keys: name, age, height, student.
     Example: {"name": "Alice", "age": 20, "height": 1.65, "student": True}
     """
-    return {}
+    return {
+        "name" : name,
+        "age" : age,
+        "height" : height,
+        "student" : student
+    }
 
 
 def age_next_year(age: int) -> int:
     """Return the person's age next year."""
-    return 0
+    return age + 1
 
 
 def is_adult(age: int) -> bool:
     """Return True if age is >= 18, else False."""
-    return False
+    return age >=18
 
 
 def format_intro(name: str, age: int) -> str:
@@ -23,11 +28,12 @@ def format_intro(name: str, age: int) -> str:
     Return a string introduction like:
     'Hello Alice, you are 20 years old.'
     """
-    return ""
+    return f"Hello {name}, you are {age} years old."
 
 
 def favorite_fruits(fruits: list) -> list:
     """
     Add a new fruit 'mango' to the list and return it.
     """
-    return []
+    fruits.append("mango")
+    return fruits
